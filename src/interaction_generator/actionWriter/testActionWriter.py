6469a8207c4f8a99f8ffe3a8b2a2edf95f6f6d53
federica_montesanti <federica.montesanti@gmail.com>
2016-11-17 17:44:20 +0100

    new version

--- src/interaction_generator/actionWriter/testActionWriter.py
+++ src/interaction_generator/actionWriter/testActionWriter.py
@@ -1,3 +1,9 @@
+from actionWriter import ActionWriter
+import glob
+import os
+
+
+
 action = {
 'NAME': 'whichcolor',
 'TEXTS': (('<*,*,es,*>','¿Cuál es tu color preferido?'), ('<*,*,*,*>','What is your favourite color?')),
@@ -12,11 +18,45 @@ action = {
 #Write the corresponding text_ and image_ files
 
 #1) text_ file will have sections texts, buttons and grammars (if present in 'action')
-
+#CHIAMARE LE FUNZIONE DELLA CLASSE ACTIONWRITER
 
 #2) image_ file will have section image (if present in 'action')
 
+my_action = {
+    'NAME': 'whichcolor',
+    'TEXTS': {'<*,*,es,*> ':'\xc2\xbfCual es tu color preferido?','<*,*,*,*> ':'What is your favourite color?'},
+    'BUTTONS': {'green':{'<*,*,es,*> ':'Verde','<*,*,*,*> ':'Green'}, 'blue':{'<*,*,es,*> ':'Azul','<*,*,*,*> ':'Blue'},'red':{'<*,*,es,*> ':'Rojo','<*,*,*,*> ':'Red'},'yellow':{'<*,*,es,*> ':'Amarillo','<*,*,*,*> ':'Yellow'},'orange':{'<*,*,es,*> ':'Naranja','<*,*,*,*> ':'Orange'},'white':{'<*,*,es,*> ':'Blanco','<*,*,*,*> ':'White'},'black':{'<*,*,es,*> ':'Negro','<*,*,*,*> ':'Black'}},
+    'GRAMMARS': {'<*,*,es,*> ':'[LOAD_GRAMMAR] frame_colornames|es','<*,*,*,*> ':'[LOAD_GRAMMAR] frame_colornames'},
+    'IMAGES': {'<*,*,es,*> ':'img/color_es.png','<*,*,*,*> ':'img/color_en.png'}
+    }
 
+
+path_write = 'file/'
+name_file = my_action['NAME']
+prefix_name = ['text_','img_']
+
+
+for prefix in prefix_name:
+    with open(path_write+prefix+name_file+'.txt', 'w') as written_file:#TOGLIERE TXT ESTENSIONE
+    # Calling/Instantiation of the class 
+        writerAct = ActionWriter(written_file,my_action)
+        
+        if prefix == 'text_':
+    
+            writerAct.writeTextSection()
+            writerAct.writeButtonsSection() # Chiedere circa le parentesi tonde
+            #writerAct.writeButton(my_action['BUTTONS'].items())
+            writerAct.writeGrammarSection()
+#    
+#     
+#             #writerAct.writeRules()
+            
+        if prefix =='img_':
+            writerAct.writeImageSection()
+            
+        
+            
+    written_file.close()
 
 
 
